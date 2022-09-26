a95e84f53c5ba698f80480294cca04e764299197
Nora Vargas <noravgs92@gmail.com>
2022-09-25 19:08:29 -0500

    updated categories images

--- src/Data/data.js
+++ src/Data/data.js
@@ -79,37 +79,37 @@ export const categories = [
   id: 1,
   name: 'Grab-and-Go',
   image:
-    'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/FastFood_BrowseHome@3x.png',
+    'https://img.freepik.com/free-vector/illustration-coffee-go_53876-8119.jpg?w=1060&t=st=1664150822~exp=1664151422~hmac=2c8df4e8a9a1060ad530bdde9672b30116ccf039e8307916f1b4ff16a0c4df2c',
 },
 {
   id: 2,
   name: 'Restaurant',
   image:
-    'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Pizza_BrowseHome@3x.png',
+    'https://img.freepik.com/premium-vector/american-breakfast-food_18591-49011.jpg?w=826',
 },
 {
   id: 3,
   name: 'Bakery',
   image:
-    'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Wings_BrowseHome@3x.png',
+    'https://img.freepik.com/premium-vector/chocolate-cupcake-white_81894-2365.jpg?w=740',
 },
 {
   id: 4,
   name: 'Craft Coffee',
   image:
-    'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Indian_BrowseHome@3x.png',
+    'https://img.freepik.com/free-vector/coffee-love-foam-with-beans-cartoon-icon-illustration_138676-2575.jpg?w=826&t=st=1664150546~exp=1664151146~hmac=53409d6c2f861455528bb87cee6d44345142d8acf9f2927bf0860d43219006d8',
 },
 {
   id: 5,
   name: 'Bookstore Coffee',
   image:
-    'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Deals_BrowseHome@3x.png',
+    'https://img.freepik.com/free-vector/stack-books-graphic-illustration_53876-8852.jpg?w=826&t=st=1664150770~exp=1664151370~hmac=3c71cf2fe1b9b639de0a07b52cfd27bd51979570ffeeeac621be39b504ecac30',
 },
 {
   id: 6,
   name: 'Pet Friendly',
   image:
-    'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/RestoRewards_BrowseHome@3x.png',
+    'https://img.freepik.com/free-vector/pet-logo-design-paw-vector-animal-shop-business_53876-136741.jpg?w=826&t=st=1664150308~exp=1664150908~hmac=f0a38819d6b679476fe335027c050429f94aa707ed5da42fa0bac7ff28cd881b',
 },
 {
   id: 7,
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -8,7 +8,7 @@ const Category = () => {
     {/* Categories*/}
     <div className='grid grid-cols-2 md:grid-cols-4 gap-6 py-6'>
        {categories.map((item, index)=> (
-        <div key={index} className='bg-gray-100 rounded-lg p-4 flex justify-between items-center'>
+        <div key={index} className='bg-white rounded-lg p-4 flex justify-between items-center'>
              <h2 className='font-bold sm:text-xl'>{item.name}</h2>
              <img src={item.image} alt={item.name} className='w-20'/>
 
