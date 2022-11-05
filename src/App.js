361642eeaf0d8613bc031d9709962d796053dbc6
Nora Vargas <noravgs92@gmail.com>
2022-11-05 13:32:30 -0500

    created seachbar component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Drinks from './components/Drinks'
 import HeadlineCards from './components/HeadlineCards'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+// import SearchBox from './components/SearchBar'
 
 function App() {
   return (
     <div className='bg-[#d8ceb3] font-poppins'>
       <Navbar />
+      {/* <SearchBox /> */}
       <Hero />
       <HeadlineCards />
       <Drinks />
